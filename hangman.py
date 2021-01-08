0758af56ae073a3552e5087ae4b455e315df4aa1
Jimmy Cullen <jimmy.cullen@gmail.com>
2021-01-08 12:09:54 +0000

    Very important ASCII art added

--- hangman.py
+++ hangman.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+
+"""Simple terminal implementation of the classic Hangman game"""
+
+def main():
+    print("Welcome to\n")
+    print("""
+     _
+    | |
+    | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __
+    | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \\
+    | | | | (_| | | | | (_| | | | | | | (_| | | | |
+    |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                       __/ |
+                      |___/
+    """)
+
+
+
+if __name__ == "__main__":
+    main()
