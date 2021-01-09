6f6e4a15ce18a12682abae109023523ecb0b5e40
Jimmy Cullen <jimmy.cullen@gmail.com>
2021-01-09 10:09:26 +0000

    First complete working version. Improvements could include type checking for only alphabetic characters and ignoring duplicate guessed characers

--- hangman.py
+++ hangman.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import random
 
-
+game_dictionary = '/home/jimmy/Code/Python/Python-Hangman/dictionary_850_words.txt'
 game_word = ""
+game_word_letters = []
 blanked_word = ""
 player_guesses = []
-no_wrong_guesses = 0
+number_wrong_guesses = 0
 
 """Simple terminal implementation of the classic Hangman game"""
 
-def main():
+def welcome():
     print("Welcome to\n")
     print("""
      _
@@ -26,48 +27,113 @@ def main():
 
 
 def random_word():
-    with open('/home/jimmy/Code/Python/Python-Hangman/dictionary_850_words.txt') as f:
+    with open(game_dictionary) as f:
 
         #variable to hold the dictionary of words used in the game
         dictionary_data = f.read().splitlines()
-        #print (dictionary_data[:8])
-
         return random.choice(dictionary_data)
 
 
 
+def check_status():
+
+    # check the current player_guesses against the game_word_letters
+    correct_guesses = 0
+
+    for letter in game_word_letters:
+        if letter in player_guesses:
+            # check the number of correct letters against the number of
+            # distinct letters in the game_word
+            correct_guesses += 1
+            if correct_guesses == len(game_word_letters):
+                return "winner"
+            else:
+                continue
+        elif letter not in player_guesses:
+            return "next_guess"
+
+
+
+
+def play():
+    #print ("check_status = " + check_status())
+    if check_status() == "next_guess" and number_wrong_guesses < 10:
+        guess = input ("\nEnter a letter to see if it is contained in the word: ")
+        player_guesses.append(guess)
+        check_guess(guess)
+
+        print ("\nYour guesses are: " + str(player_guesses))
+        play()
+
+    elif check_status() == "next_guess" and number_wrong_guesses == 10:
+
+        print("Too bad partner, you lost. Better luck next time :-)")
+        exit()
+
+    elif check_status() == "winner":
+
+        print("""
+                                            _         _       _   _
+                                           | |       | |     | | (_)
+             ___ ___  _ __   __ _ _ __ __ _| |_ _   _| | __ _| |_ _  ___  _ __  ___
+            / __/ _ \| '_ \ / _` | '__/ _` | __| | | | |/ _` | __| |/ _ \| '_ \/ __|
+           | (_| (_) | | | | (_| | | | (_| | |_| |_| | | (_| | |_| | (_) | | | \__ \\
+            \___\___/|_| |_|\__, |_|  \__,_|\__|\__,_|_|\__,_|\__|_|\___/|_| |_|___/
+                             __/ |
+                            |___/
+
+        Congrats, you beat me ;-P""")
+        exit()
+
+
+
+
 def check_guess(guess):
+
+
+    global number_wrong_guesses
+    global blanked_word
+
     #check if the guessed letter is in the hidden word
     if guess in game_word:
 
         #find out how many times the letter appears
         no_occurences = game_word.count(guess)
         if no_occurences == 1:
-            print ("Your guess appears once in the word.")
+            print ("\nYour guess appears once in the word.\n")
         else:
-            print ("Your guess appears " + str(no_occurences) + " times in the word.")
+            print ("\nYour guess appears " + str(no_occurences) + " times in the word.\n")
 
         #replace the underscore in the blanked word with the correctly guessed letter
         substring_idx = 0
-        while no_occurences:
-            print ("substring_idx = " + str(substring_idx))
-            substring_idx = game_word.find(guess, substring_idx+1)
-            print ("substring_idx = " + str(substring_idx))
+        while no_occurences > 0:
+            ###print ("substring_idx = " + str(substring_idx))
+
+            if substring_idx == 0:
+                #case when the guessed letter is the first occurence of the letter in the game_word
+                substring_idx = game_word.find(guess, substring_idx)
+            else:
+                #case when the guessed letter is not the first occurence of the letter of the game_word
+                substring_idx = game_word.find(guess, substring_idx+1)
+
+
+            ###print ("substring_idx = " + str(substring_idx))
             no_occurences -= 1
-            print("Number of remaining occurences: " + str(no_occurences))
+            ###print("Number of remaining occurences: " + str(no_occurences))
 
             blanked_word_list = list(blanked_word)
             blanked_word_list[(substring_idx*2)] = guess
             blanked_word = "".join(blanked_word_list)
             print (blanked_word)
-        #while
+
 
     else:
-        print("Unlucky, your guess was incorrect.")
+        print("\nUnlucky, your guess was incorrect.")
+        ###print (number_wrong_guesses)
 
         #enter a elif block to determine which animation to draw
 
-        if no_wrong_guesses == 0:
+        if number_wrong_guesses == 0:
 
             print("""
 
@@ -80,9 +146,9 @@ def check_guess(guess):
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 1:
+        elif number_wrong_guesses == 1:
             print("""
 
                   |
@@ -93,26 +159,12 @@ def check_guess(guess):
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 2:
-            print("""
-              +---+
-                  |
-                  |
-                  |
-                  |
-                  |
-            =========
-            """)
-
-            no_wrong_guesses += 1
-
-        elif no_wrong_guesses == 3:
-
+        elif number_wrong_guesses == 2:
             print("""
               +---+
-              |   |
+                  |
                   |
                   |
                   |
@@ -120,37 +172,51 @@ def check_guess(guess):
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 4:
+        elif number_wrong_guesses == 3:
 
             print("""
               +---+
               |   |
-              O   |
+                  |
                   |
                   |
                   |
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 5:
+        elif number_wrong_guesses == 4:
 
             print("""
               +---+
               |   |
               O   |
+                  |
+                  |
+                  |
+            =========
+            """)
+
+            number_wrong_guesses += 1
+
+        elif number_wrong_guesses == 5:
+
+            print("""
+              +---+
+              |   |
+              O   |
               |   |
                   |
                   |
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 6:
+        elif number_wrong_guesses == 6:
 
             print("""
               +---+
@@ -162,9 +228,9 @@ def check_guess(guess):
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 7:
+        elif number_wrong_guesses == 7:
 
             print("""
               +---+
@@ -176,9 +242,9 @@ def check_guess(guess):
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 8:
+        elif number_wrong_guesses == 8:
 
             print("""
               +---+
@@ -190,9 +256,9 @@ def check_guess(guess):
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
-        elif no_wrong_guesses == 9:
+        elif number_wrong_guesses == 9:
 
             print("""
               +---+
@@ -204,7 +270,7 @@ def check_guess(guess):
             =========
             """)
 
-            no_wrong_guesses += 1
+            number_wrong_guesses += 1
 
 
 
@@ -212,18 +278,31 @@ def check_guess(guess):
 
 if __name__ == "__main__":
 
-    main()
-    game_word = random_word().lower()
-    print(game_word)
+    # say hello
+    welcome()
 
-    print("The word you must guess looks like this:")
+    # select a word for the game
+    game_word = random_word().lower()
+
+
+
+    # turn the game_word string into a list of individual characters
+    # and add unique members to the game_word_letters list
+    for letter in list(game_word):
+        # check if letter exists in game_word_letters or not
+        if letter not in game_word_letters:
+            game_word_letters.append(letter)
+
+    ###print (game_word)
+    ###print (game_word_letters)
+
+    # generate the blanked_word and display
+    print("The word you must guess looks like this:\n")
     blanked_word = "_ " * len(game_word)
     print (blanked_word)
 
-    guess = input ("Enter a letter to see if it is contained in the word: ")
-    #guess = "e".lower()
-    player_guesses.append(guess)
 
-    check_guess(guess)
 
-    print ("\nYour guesses are: " + str(player_guesses))
+    # main function of the game
+    # if set(player_guesses) < set(game_word_letters):
+    play()
